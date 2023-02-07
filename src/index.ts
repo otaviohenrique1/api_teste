444a8b7666092428caff062bc38ec24889e94d70
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-02-06 21:49:51 -0300

    Commit 1

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { listaProdutos } from "../utils/listaProdutos";
+
+const app = express();
+app.use(express.json());
+app.use(cors);
+
+app.get("/produtos", async function (req: Request, res: Response) {
+  res.json(listaProdutos)
+})
+
+app.listen(3000);
+
+/* Link de teste => http://localhost:3000/produtos */
